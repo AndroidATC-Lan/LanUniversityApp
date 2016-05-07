ee9153df543090f72c6359eabd9bf22c24124f07
Carlos Augusto <csousafilho@gmail.com>
2016-05-07 09:41:49 -0300

    AND-1-MENU

--- app/src/main/java/br/com/lanuniversity/lanuniversity/SplashActivity.java
+++ app/src/main/java/br/com/lanuniversity/lanuniversity/SplashActivity.java
@@ -1,7 +1,25 @@
 package br.com.lanuniversity.lanuniversity;
 
-/**
- * Created by csousafilho on 02/05/16.
- */
-public class SplashActivity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getBaseContext(), MainActivity.class));
+                finish();
+            }
+        },2000);
+    }
 }
