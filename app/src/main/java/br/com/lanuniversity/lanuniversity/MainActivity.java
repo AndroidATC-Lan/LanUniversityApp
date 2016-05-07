ed613814333bc6d5dc01773b8dd166ad6e231b42
Carlos Augusto <csousafilho@gmail.com>
2016-05-07 09:45:38 -0300

    AND-1-MENU

--- app/src/main/java/br/com/lanuniversity/lanuniversity/MainActivity.java
+++ app/src/main/java/br/com/lanuniversity/lanuniversity/MainActivity.java
@@ -1,15 +1,9 @@
 package br.com.lanuniversity.lanuniversity;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.Fragment;
-import android.transition.TransitionManager;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -17,6 +11,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -92,15 +87,19 @@ public class MainActivity extends AppCompatActivity
 
         switch(id){
             case R.id.nav_sobre:
+                toast("Você clicou em Sobre");
                 fragment = new SobreFragment();
                 break;
             case R.id.nav_cursos:
+                toast("Você clicou em Cursos");
                 fragment = new CursosFragment();
                 break;
             case R.id.nav_instrutores:
+                toast("Você clicou em Instrutores");
                 fragment = new InstrutoresFragment();
                 break;
             case R.id.nav_como_chegar:
+                toast("Você clicou em Como chegar");
                 fragment = new ComoChegarFragment();
                 break;
         }
@@ -112,6 +111,12 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    private void toast (String s) {
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+
 
     @Override
     public void onFragmentInteraction(Uri uri) {
