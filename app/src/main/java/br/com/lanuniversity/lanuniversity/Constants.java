bdbfd7cec7cbbc8d71eeec9b7dba9c07c1c84f50
Matheus Miranda <matheus.sm86@gmail.com>
2016-05-09 17:43:16 -0300

    Criar classe para constants de API, email site Lan

---
+++ app/src/main/java/br/com/lanuniversity/lanuniversity/Constants.java
@@ -0,0 +1,14 @@
+package br.com.lanuniversity.lanuniversity;
+
+/**
+ * Created by Matheus Miranda on 5/9/16.
+ */
+public final class Constants {
+    public static final String cEMAIL_LAN = "atendimento@lanuniversity.com.br";
+    public static final String cSITE_LAN = "http://www.lanuniversity.com.br/";
+    public static final String cAPI_SOBRE = "http://ragazzid.com.br/v1/site/sobre";
+    public static final String cAPI_CURSOS = "http://ragazzid.com.br/v1/site/cursos";
+    public static final String cAPI_INSTRUTORES = "http://ragazzid.com.br/v1/site/instrutores";
+    
+    private Constants() {}
+}
