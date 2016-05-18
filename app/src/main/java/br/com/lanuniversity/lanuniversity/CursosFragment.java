7d8538c71f9bbc29b528bafd8c946656830156c8
Haraldo Filho <haraldo.filho@gmail.com>
2016-05-17 21:24:29 -0300

    Removido parâmetros desnecessários nas classes Fragment. Renomeada variável json_server_address para jsonServerAddress.

--- app/src/main/java/br/com/lanuniversity/lanuniversity/CursosFragment.java
+++ app/src/main/java/br/com/lanuniversity/lanuniversity/CursosFragment.java
@@ -23,14 +23,6 @@ import java.util.concurrent.ExecutionException;
  * create an instance of this fragment.
  */
 public class CursosFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -38,38 +30,21 @@ public class CursosFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment CursosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static CursosFragment newInstance(String param1, String param2) {
+    public static CursosFragment newInstance() {
         CursosFragment fragment = new CursosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
-        String json_server_address = getResources().getText(R.string.json_server_address) +
+        String jsonServerAddress = getResources().getText(R.string.json_server_address) +
                 "/" + getResources().getText(R.string.file_name_cursos);
-        Log.d("Server address", json_server_address);
+        Log.d("Server address", jsonServerAddress);
 
         HttpJsonConnection jsonConnection = new HttpJsonConnection();
-        jsonConnection.execute(json_server_address);
+        jsonConnection.execute(jsonServerAddress);
         try {
             String jsonString = jsonConnection.get();
             Cursos jsonCursos = new Cursos();
--- app/src/main/java/br/com/lanuniversity/lanuniversity/SobreFragment.java
+++ app/src/main/java/br/com/lanuniversity/lanuniversity/SobreFragment.java
@@ -22,14 +22,6 @@ import java.util.concurrent.ExecutionException;
  * create an instance of this fragment.
  */
 public class SobreFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
@@ -37,38 +29,21 @@ public class SobreFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment SobreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static SobreFragment newInstance(String param1, String param2) {
+    public static SobreFragment newInstance() {
         SobreFragment fragment = new SobreFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
         return fragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
 
-        String json_server_address = getResources().getText(R.string.json_server_address) +
+        String jsonServerAddress = getResources().getText(R.string.json_server_address) +
                 "/" + getResources().getText(R.string.file_name_sobre);
-        Log.d("Server address", json_server_address);
+        Log.d("Server address", jsonServerAddress);
 
         HttpJsonConnection jsonConnection = new HttpJsonConnection();
-        jsonConnection.execute(json_server_address);
+        jsonConnection.execute(jsonServerAddress);
         try {
             String jsonString = jsonConnection.get();
             Sobre jsonSobre = new Sobre();
